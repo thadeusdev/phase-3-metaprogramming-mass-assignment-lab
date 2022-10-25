7d416f1591b780efed4ca5b74ebbdf57872c9814
thadeus <thadeusnyariki94@gmail.com>
2022-10-26 02:42:20 +0300

    Done assignment

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,22 @@
 class Person
   # your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      # create a getter and setter by calling the attr_accessor method
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+  end
 end
+
+bob_attributes = { name: "Bob", hair_color: "Brown" }
+bob = Person.new(bob_attributes)
+p bob.name       # => "Bob"
+p bob.hair_color # => "Brown"
+
+susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+
+susan = Person.new(susan_attributes)
+p susan.name      # => "Susan"
+p susan.height    # => "5'11""
+p susan.eye_color # => "Green"
